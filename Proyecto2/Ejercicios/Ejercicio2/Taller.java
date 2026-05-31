158ff88c0f84ce0d44050d1198c6add1c73df68d
IchKote <167239730+IchKote@users.noreply.github.com>
2026-05-31 17:31:49 -0400

    Enhance comments and documentation in Taller.java
    Added comments for clarity and improved code documentation.

--- Proyecto2/Ejercicios/Ejercicio2/Taller.java
+++ Proyecto2/Ejercicios/Ejercicio2/Taller.java
@@ -1,10 +1,17 @@
+// Fiona Catalina Castro Villarroel 21501220-4
+// José-Tomás Guzmán  Ilabel 21.132.176-8
+// Máximo Eduardo González Arriagada 21.978.604-2
+// Stefano Negroni Postiglione  21.945.448-1
+// Pablo Antonio Labra Jabre 21.280.591-2
+// Isabella Victoria Quintero González 25.868.144-4
+
 package Ejercicio2;
 import java.util.ArrayList;
 
 public class Taller {
     //Atributos
     private String nombre;
-    private ArrayList<Estudiante> estudiantes;
+    private ArrayList<Estudiante> estudiantes; //Arreglo vectorial de estudiantes
     //Constructores
     public Taller(String nombre) {
         this.nombre = nombre;
@@ -30,6 +37,7 @@ public class Taller {
     public void agregarEstudiante(Estudiante estudiante) {
         estudiantes.add(estudiante);
     }
+    //método para mostrar los estudiantes pertenecientes al taller, accediento a su nombre mediante su getter
     public void mostrarEstudiantes() {
         System.out.println("Estudiantes en el taller " + getNombre() + ":");
         for (Estudiante estudiante : estudiantes) {
@@ -37,6 +45,7 @@ public class Taller {
         }
         System.out.println("");
     }
+    //método para eliminar a un estudiante del taller mediante un nombre como parametro de entrada
     public boolean removerEstudiantePorNombre(String nombre) {
         for (Estudiante estudiante : estudiantes) {
             if (estudiante.getNombre().equalsIgnoreCase(nombre)) {
