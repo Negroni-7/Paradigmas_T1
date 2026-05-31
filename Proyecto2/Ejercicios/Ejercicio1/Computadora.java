3afe2c9f6f5fa41da4b379a8805d0fbcd0deb6be
IchKote <167239730+IchKote@users.noreply.github.com>
2026-05-31 17:28:52 -0400

    Improve comments in Computadora.java
    Updated comments for clarity in Computadora class.

--- Proyecto2/Ejercicios/Ejercicio1/Computadora.java
+++ Proyecto2/Ejercicios/Ejercicio1/Computadora.java
@@ -1,3 +1,10 @@
+// Fiona Catalina Castro Villarroel 21501220-4
+// José-Tomás Guzmán  Ilabel 21.132.176-8
+// Máximo Eduardo González Arriagada 21.978.604-2
+// Stefano Negroni Postiglione  21.945.448-1
+// Pablo Antonio Labra Jabre 21.280.591-2
+// Isabella Victoria Quintero González 25.868.144-4
+
 public class Computadora {
     //Atributos
     private String marca;
@@ -28,7 +35,7 @@ public class Computadora {
         this.procesador = procesador;
     }
 
-    //método para mostrar la información de la computadora
+    //método para mostrar la información de la computadora accediendo a sus getters, llama a la funcion mostrarInformacion perteneciente al procesador
     public void mostrarInformacion() {
         System.out.println("Marca: " + getMarca());
         System.out.println("Información del Procesador:");
