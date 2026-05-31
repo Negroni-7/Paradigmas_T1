254086c313665fea379a78e01278beb251a8efe5
IchKote <167239730+IchKote@users.noreply.github.com>
2026-05-31 17:27:30 -0400

    Refactor comments and update mostrarInformacion method
    Updated comments for clarity and added getter usage in mostrarInformacion method.

--- Proyecto2/Ejercicios/Ejercicio1/Procesador.java
+++ Proyecto2/Ejercicios/Ejercicio1/Procesador.java
@@ -1,3 +1,9 @@
+// Fiona Catalina Castro Villarroel 21501220-4
+// José-Tomás Guzmán  Ilabel 21.132.176-8
+// Máximo Eduardo González Arriagada 21.978.604-2
+// Stefano Negroni Postiglione  21.945.448-1
+// Pablo Antonio Labra Jabre 21.280.591-2
+// Isabella Victoria Quintero González 25.868.144-4
 
 public class Procesador {
     
@@ -33,7 +39,7 @@ public class Procesador {
         this.frecuenciaGHz = frecuenciaGHz;
     }
 
-    //método para mostrar la información del procesador
+    //método para mostrar la información del procesador utilizando los getters
     public void mostrarInformacion() {
         System.out.println("Modelo: " + getModelo());
         System.out.println("Frecuencia: " + getFrecuenciaGHz() + " GHz");
